507abab52d6d450a0bb54e97799a7a9f65c3f3a1
Sameer yadav <sameer.manoj2005@gmail.com>
2026-02-14 10:51:41 +0530

    second commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
-import connectDB from './config/Db.js';
+// import connectDB from './config/Db.js';
 import path, { resolve } from 'path';
 import { fileURLToPath } from "url";
 dotenv.config()
@@ -13,11 +13,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__filename);
 console.log(__dirname);
-app.set('view engine', path.join(__dirname,"views"))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,"public")))
 const url = process.env.MONGODB_URL;
 
 
 // connectDB(url)
-new Promise((resolve,reject)=>resolve())
-.then(()=>app.listen(5000,()=>console.log("Server running on port 5000"))).catch((err)=>console.log("Server stop !"))
+// new Promise((resolve,reject)=>resolve())
+// .then(()=>app.listen(5000,()=>console.log("Server running on port 5000"))).catch((err)=>console.log("Server stop !"))
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
